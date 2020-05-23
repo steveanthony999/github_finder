b902588144bb4b6165ba3188ca7e1a11faa480b2
Steven Woodward <steveanthony999@gmail.com>
2020-05-22 22:56:12 -0700

    imported old project to this

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -1,27 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github'
-  };
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='bg-primary'>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'>HOME</Link>
+        </li>
+        <li>
+          <Link to='/about'>ABOUT</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired
-  };
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+};
 
-  render() {
-    return (
-      <div className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon}></i>
-          {this.props.title}
-        </h1>
-      </div>
-    );
-  }
-}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
